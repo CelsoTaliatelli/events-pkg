aba3bc786b61ca5f970176c914839c100fe5827f
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-10-30 19:41:21 -0300

    tes: testando register

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -40,14 +40,20 @@ type EventDispatcherTestSuite struct {
 	handler         TestEventHandler
 	handler2        TestEventHandler
 	handler3        TestEventHandler
-	eventDispatcher EventDispatcher
+	eventDispatcher *EventDispatcher
 }
 
 func (suite *EventDispatcherTestSuite) SetupTest() {
-	suite.eventDispatcher = *NewEventDispatcher()
-	suite.handler = TestEventHandler{}
-	suite.handler2 = TestEventHandler{}
-	suite.handler3 = TestEventHandler{}
+	suite.eventDispatcher = NewEventDispatcher()
+	suite.handler = TestEventHandler{
+		ID: 1,
+	}
+	suite.handler2 = TestEventHandler{
+		ID: 2,
+	}
+	suite.handler3 = TestEventHandler{
+		ID: 3,
+	}
 	suite.event = TestEvent{Name: "teste", Payload: "test"}
 	suite.event2 = TestEvent{Name: "test2", Payload: "test2"}
 
@@ -55,6 +61,16 @@ func (suite *EventDispatcherTestSuite) SetupTest() {
 
 func (suite *EventDispatcherTestSuite) TestEventDispatcherTest() {
 	assert.True(suite.T(), true)
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcherTest_Register() {
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
 }
 
 func TestSuite(t *testing.T) {
